4c2c90f66d8abd2bdbe16bdedd9f180b6213c3c2
xiaominghe93 <xiaominghe93@gmail.com>
2020-05-21 19:44:32 +0800

    完成了登录

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      // 一般情况点击退出 先清楚token然后返回到登录页
+      this.$router.push('/login')
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 引入全局样式
+import './assets/css/global.css'
+// 引入矢量图样式
+import './assets/font/iconfont.css'
 
+// 引入设置的axios
+import http from './http'
+
+Vue.prototype.$http = http
 Vue.config.productionTip = false
 
 new Vue({
